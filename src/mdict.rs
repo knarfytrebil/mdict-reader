f17bb0966a2a828cb5dd3a834c18e4f6faccfc21
Cuichen Li <cuichen.li94@gmail.com>
2020-03-08 16:30:21 +1100

    Get offset

--- src/mdict.rs
+++ src/mdict.rs
@@ -6,19 +6,25 @@ use std::convert::TryInto;
 use failure::Error;
 
 pub struct MdictHeader {
-
+    offset: u64
 }
 
 pub fn read_header(file_name: &str) -> Result<MdictHeader, Error>{
-    let mut file = File::open(file_name)?;
-    let size = file.read_uint::<BigEndian>(4)?;
-    let mut header: Vec<u8> = vec![0 ; size as usize];
-    for i in 0..size {
-        header[i as usize] = file.read_u8()?;
+    let offset: u64;
+    {
+        let mut file = File::open(file_name)?;
+        let size = file.read_uint::<BigEndian>(4)?;
+        let mut header: Vec<u8> = vec![0 ; size as usize];
+        for i in 0..size {
+            header[i as usize] = file.read_u8()?;
+        }
+        let actual_checksum = adler32(&header[..])?;
+        let expected_checksum = file.read_uint::<LittleEndian>(4)? as u32;
+        assert_eq!(actual_checksum, expected_checksum);
+        offset = file.seek(SeekFrom::Current(0))?;
     }
-    let actual_checksum = adler32(&header[..]).unwrap();
-    let checksum = file.read_uint::<LittleEndian>(4)?;
-    println!("Actual check sum {}", actual_checksum);
-    println!("Checksum {}", checksum);
-    Ok(MdictHeader{})
+
+    Ok(MdictHeader{
+        offset
+    })
 }
